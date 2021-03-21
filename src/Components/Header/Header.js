d800ffd1399a3fd226a0d1b62b02d8c05d9d4670
jahidhasan6484 <jahidhasan6484@gmail.com>
2021-03-21 14:44:56 +0600

    six

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -39,27 +39,6 @@ const Header = () => {
                 </div>
             </div>
         </nav>
-
-        // <Navbar bg="light" expand="lg">
-        //     <Container>
-        //         <div className="col-md-6">
-        //             <Link className="navbar-brand h3 m-0" to="/">Let's<span className="text-primary">GO!</span></Link>
-        //         </div>
-        //         <div className="col-md-6">
-        //             <Nav className="mr-auto">
-        //                 <Nav.Link as={Link} className="text-dark" to="/home">Home</Nav.Link>
-        //                 <Nav.Link as={Link} className="text-dark" to="/destinations">Destination</Nav.Link>
-        //                 <Nav.Link as={Link} className="text-dark" >Blog</Nav.Link>
-        //                 <Nav.Link as={Link} className="text-dark" >Contact</Nav.Link>
-        //                 {
-        //                     loggedInUser.email ? <Nav.Link as={Link} className="text-dark" to="/profile">{loggedInUser.name || loggedInUser.email}</Nav.Link> :
-        //                     <Nav.Link as={Link} className="btn btn-info" to="/login">login</Nav.Link>
-        //                 }
-
-        //             </Nav>
-        //         </div>
-        //     </Container>
-        // </Navbar>
     );
 };
 
